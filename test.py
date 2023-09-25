222e2c95b7c74d34f047e043dd3c5c16a4a7cc4c
davgo0103 <davvgo@gmail.com>
2023-09-25 11:14:36 +0800

    Update

--- test.py
+++ test.py
@@ -1,36 +1,8 @@
-from scapy.all import *
-import time
+import scapy.all as scapy
+import scapy.contrib.modbus as mb
 
-# Modbus ADU
-class ModbusTCP(Packet):
-    name = "Modbus/TCP"
-    fields_desc = [
-        ShortField("Transaction_Identifier", 1),
-        ShortField("Protocol_Identifier", 0),
-        ShortField("Length", 6),
-        XByteField("Unit_Identifier", 1),
-    ]
-
-# Modbus PDU
-class Modbus(Packet):
-    name = "Modbus"
-    fields_desc = [
-        XByteField("Function_Code", 3),  
-        ShortField("Reference_Number", 0),
-        ShortField("Word_Count", 63),
-    ]
-
-
-s = socket.socket()
-s.connect(("192.168.50.197", 502))
-ss = StreamSocket(s, Raw)
-
-try:
-    response = ss.sr1(Raw(ModbusTCP()/Modbus()))
-    if response:
-        response_hexdump = hexdump(response, dump=True)
-        print(response_hexdump)
-
-        time.sleep(1)
-except KeyboardInterrupt:
-    pass
+for pkt in scapy.PcapReader("write.pcapng"):
+    if mb.ModbusADUResponse in pkt:
+        pkt.show()
+    if mb.ModbusPDU06WriteSingleRegisterResponse in pkt:
+        pkt.show()
